08fb520e5552f065e2846c09d689fc9b1017ccdc
Josh Hung <jhung@netbasequid.com>
2023-09-25 18:47:03 +0800

    badge

---
+++ components/Badge.tsx
@@ -0,0 +1,7 @@
+export const Badge = () => {
+  return (
+    <div className="inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
+      20
+    </div>
+  );
+};
--- components/Chatbar/components/Conversation.tsx
+++ components/Chatbar/components/Conversation.tsx
@@ -18,6 +18,7 @@ import { Conversation } from '@/types/chat';
 
 import HomeContext from '@/pages/api/home/home.context';
 
+import { Badge } from '@/components/Badge';
 import SidebarActionButton from '@/components/Buttons/SidebarActionButton';
 import ChatbarContext from '@/components/Chatbar/Chatbar.context';
 
@@ -37,6 +38,8 @@ export const ConversationComponent = ({ conversation }: Props) => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [isRenaming, setIsRenaming] = useState(false);
   const [renameValue, setRenameValue] = useState('');
+
+  const isConversationSelected = selectedConversation?.id === conversation.id;
 
   const handleEnterDown = (e: KeyboardEvent<HTMLDivElement>) => {
     if (e.key === 'Enter') {
@@ -102,7 +105,7 @@ export const ConversationComponent = ({ conversation }: Props) => {
 
   return (
     <div className="relative flex items-center">
-      {isRenaming && selectedConversation?.id === conversation.id ? (
+      {isRenaming && isConversationSelected ? (
         <div className="flex w-full items-center gap-3 rounded-lg bg-[#343541]/90 p-3">
           <IconMessage size={18} />
           <input
@@ -118,11 +121,7 @@ export const ConversationComponent = ({ conversation }: Props) => {
         <button
           className={`flex w-full cursor-pointer items-center gap-3 rounded-lg p-3 text-sm transition-colors duration-200 hover:bg-[#343541]/90 ${
             messageIsStreaming ? 'disabled:cursor-not-allowed' : ''
-          } ${
-            selectedConversation?.id === conversation.id
-              ? 'bg-[#343541]/90'
-              : ''
-          }`}
+          } ${isConversationSelected ? 'bg-[#343541]/90' : ''}`}
           onClick={() => handleSelectConversation(conversation)}
           disabled={messageIsStreaming}
           draggable="true"
@@ -131,38 +130,36 @@ export const ConversationComponent = ({ conversation }: Props) => {
           <IconMessage size={18} />
           <div
             className={`relative max-h-5 flex-1 overflow-hidden text-ellipsis whitespace-nowrap break-all text-left text-[12.5px] leading-3 ${
-              selectedConversation?.id === conversation.id ? 'pr-12' : 'pr-1'
+              isConversationSelected ? 'pr-12' : 'pr-1'
             }`}
           >
             {conversation.name}
           </div>
+          {!isConversationSelected && <Badge />}
         </button>
       )}
 
-      {(isDeleting || isRenaming) &&
-        selectedConversation?.id === conversation.id && (
-          <div className="absolute right-1 z-10 flex text-gray-300">
-            <SidebarActionButton handleClick={handleConfirm}>
-              <IconCheck size={18} />
-            </SidebarActionButton>
-            <SidebarActionButton handleClick={handleCancel}>
-              <IconX size={18} />
-            </SidebarActionButton>
-          </div>
-        )}
+      {(isDeleting || isRenaming) && isConversationSelected && (
+        <div className="absolute right-1 z-10 flex text-gray-300">
+          <SidebarActionButton handleClick={handleConfirm}>
+            <IconCheck size={18} />
+          </SidebarActionButton>
+          <SidebarActionButton handleClick={handleCancel}>
+            <IconX size={18} />
+          </SidebarActionButton>
+        </div>
+      )}
 
-      {selectedConversation?.id === conversation.id &&
-        !isDeleting &&
-        !isRenaming && (
-          <div className="absolute right-1 z-10 flex text-gray-300">
-            <SidebarActionButton handleClick={handleOpenRenameModal}>
-              <IconPencil size={18} />
-            </SidebarActionButton>
-            <SidebarActionButton handleClick={handleOpenDeleteModal}>
-              <IconTrash size={18} />
-            </SidebarActionButton>
-          </div>
-        )}
+      {isConversationSelected && !isDeleting && !isRenaming && (
+        <div className="absolute right-1 z-10 flex text-gray-300">
+          <SidebarActionButton handleClick={handleOpenRenameModal}>
+            <IconPencil size={18} />
+          </SidebarActionButton>
+          <SidebarActionButton handleClick={handleOpenDeleteModal}>
+            <IconTrash size={18} />
+          </SidebarActionButton>
+        </div>
+      )}
     </div>
   );
 };
